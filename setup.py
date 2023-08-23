b3e0d2db214e3ff18487321e6a643205b0f4ba5e
Syed Hashim <hashim.muhammad9@gmail.com>
2023-08-23 12:33:03 +0500

    update package name

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open("requirements.txt", "r") as f:
     required = f.read().splitlines()
 
     setuptools.setup(
-        name="docker-clean",
+        name="dockclean",
         version="1.0.0-beta",
         author="Muhammad Hashim",
         author_email="hashim.muhammad9@gmail.com",
         description="Tool to clean docker images and containers",
         packages=setuptools.find_packages(),
         install_requires=required,
-        scripts=["bin/docker-clean"],
+        scripts=["bin/dockclean"],
     )
